3b5ed1717f52810ac0b096e6e15654735696f5cf
Fuwy <fwy423@gmail.com>
2017-03-11 18:21:11 -0500

    twitter streaming

--- collect tweet/twitter_streaming.py
+++ collect tweet/twitter_streaming.py
@@ -1,25 +1,29 @@
 import tweepy
 import time
+import json
 
-ckey = "ZlPgE73ivGdJAl3ROeNHc1RKJ"
-csecret = "s9FyEOMOmjckzmtfuwm9Iw6NGiFk8tMxmEYllxYzC1rMd4XDVe"
-atoken = "784480113459093504-izVnLvSy8MAE5e84tYD04WY1CCaKD4L"
-asecret = "nhQiQaRxFrUyNLtCOjeLO7wBVx9XcvSG8OqCAtg5RhqmJ"
+ckey = "SoOGXhd8y9Hh9NG1qKrIwX8VN"
+csecret = "PmogNRIh6zjE0bw2SzZmQOw7Er4xWjKmSvmphgbYiG2q4eEWUX"
+atoken = "768635579647033348-4AB8vyLVUgz26ZSU8ue2rd2CAXBdLml"
+asecret = "l0Lhu4u2gmKMACd3Dqygg3CtaKZ8Ab6JoVMf95JmBAY9v"
 
-auth = tweepy.OAuthHandler(ckey, csecret)
-auth.set_access_token(atoken, asecret)
 
-api = tweepy.API(auth)
-public_tweets = api.home_timeline()
+def center_location(location):
+    assert len(location) == 4
+    x = location[0][0] + location[1][0] + location[2][0] + location[3][0]
+    y = location[0][1] + location[1][1] + location[2][1] + location[3][1]
+    return [x / 4, y / 4]
 
 
 class MyStreamListener(tweepy.StreamListener):
     def __init__(self, limit_mode=True, time_limit=60, log_file=None):
-        if (limit_mode):
+        print("Initialization...")
+        if limit_mode:
+            print("Time Limit Mode: ", time_limit, "sec")
             self.start_time = time.time()
             self.limit = time_limit
 
-        if (log_file is None):
+        if log_file is None:
             # creat a new log file
             self.log_file = open('streaming.log', 'a', encoding='utf8')
         else:
@@ -36,5 +40,40 @@ class MyStreamListener(tweepy.StreamListener):
     def on_data(self, data):
         # parse the twitter and extract the geolocation information
         # Reference: https://dev.twitter.com/overview/api/tweets
+        print("+++++++++++++")
+        if (time.time() - self.start_time) < self.limit:
+            try:
+                json_msg = json.loads(data)
 
-        print(data.text)
+                if json_msg.get("place") is not None:
+                    location = json_msg["place"]["bounding_box"]["coordinates"]
+                    #                 location = json_msg["coordinates"]["coordinates"]
+                    text = json_msg["text"]
+                    timestamp = json_msg["timestamp_ms"]
+                    user_name = json_msg["user"]["screen_name"]
+                    print('location:', location[0][0])
+                    print('timestamp:', timestamp)
+                    print('user_name:', user_name)
+                    print('text:', text)
+
+            except Exception as e:
+                print("Error: " + str(e))
+                self.log_file.write("Error: " + str(e))
+
+            return True
+        else:
+            self.log_file.close()
+            return False
+
+
+if __name__ == '__main__':
+
+    auth = tweepy.OAuthHandler(ckey, csecret)
+    auth.set_access_token(atoken, asecret)
+
+    api = tweepy.API(auth)
+    with open("streaming.log", 'a', encoding="utf-8") as log:
+        print("start streaming...")
+        myStreamListener = MyStreamListener(limit_mode=True, time_limit=10, log_file=log)
+        myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+        myStream.filter(track=['hello'])
