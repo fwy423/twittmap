d188938758cd87b68c55801677dc1e5ba4ecc748
Zebin Wang <wangzebintony@gmail.com>
2017-03-11 20:29:44 -0500

    fix script cache problem

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,18 @@
 
 function initMap() {
-  var columbia = {lat: 40.8075, lng: -73.9626};
-// Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center : columbia,
-    scrollwheel : false,
-    zoom : 4
-  });
+        var myLatLng = {lat: 40.8075, lng: -73.9626};
 
-  // var imsage = '../img/pin.png';
-
-  var marker = new google.maps.Marker({
-          map: map,
-          position: columbia,
-          // icon: image
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: myLatLng,
+          scrollwheel: false,
+          zoom: 4
         });
 
-}
+        // Create a marker and set its position.
+        var marker = new google.maps.Marker({
+          map: map,
+          position: myLatLng,
+          title: 'Hello World!'
+        });
+      }
