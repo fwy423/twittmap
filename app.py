c5ff1bb174cb4740cabca1d8c303a96bdb0581ad
Zebin Wang <wangzebintony@gmail.com>
2017-03-11 21:51:50 -0500

    add search

--- app.py
+++ app.py
@@ -5,6 +5,19 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-	
+
+
+# pre-load fixed tweets
+def pre_load_fixed_data():
+    keywords = ["music", "food", "sport", "show", "movie", "car", "commercial", "party", "war", "hello"]
+    data = DataReader()
+    return data.read("static/data/tweets.txt", keywords)
+
+tweets_json = pre_load_fixed_data()
+@app.route("/searchf/<keyword>")
+def searchf(keyword = None):
+	if keyword is None:
+
+
 if __name__ == "__main__":
     app.run()
