aaf5bbf1f42c809f5d5623851ced5499ab66da1e
Fuwy <fwy423@gmail.com>
2017-03-12 19:34:25 -0400

    flask application

--- application.py
+++ application.py
@@ -4,21 +4,21 @@ from flask import Flask, render_template, send_file,jsonify
 sys.path.append("collect_tweet/")
 from twitter_elasticsearch_util import clear, search, location_search
 
-app = Flask(__name__)
+application = Flask(__name__)
 elastic_host = "search-twittmap-wf-tos22nd6jgkyhdhvbptnb4pv7a.us-east-1.es.amazonaws.com"
 keywords = ["lunch", "food", "dinner", "eat", "desert", "delicious", "drinks", "bar", "restaurant", "breakfast"]
 
 
-@app.route("/")
+@application.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/clear/<keyword>")
+@application.route("/clear/<keyword>")
 def clear(keyword=None):
     return clear(elastic_host, keyword)
 
 
-@app.route("/searchf/<keyword>")
+@application.route("/searchf/<keyword>")
 def searchf(keyword):
     if keyword in keywords:
         result = search(elastic_host, keyword)
@@ -30,10 +30,10 @@ def searchf(keyword):
     return jsonify(result)
 
 
-@app.route('/images/<filename>')
+@application.route('/images/<filename>')
 def get_image(filename=None):
     return send_file('static/images/' + filename, mimetype='image/png')
 
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0')
+    application.run(host = '0.0.0.0')
